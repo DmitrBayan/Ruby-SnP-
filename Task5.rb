12ec3da0229c49d474f0039d13113f093d9f3f9d
DmitrBayan <55316650+DmitrBayan@users.noreply.github.com>
2019-09-23 19:22:07 +0300

    Update Task5.rb

--- Task5.rb
+++ Task5.rb
@@ -1,10 +1,10 @@
 def date_in_future(int=0)
   time = Time.now
   if int.class != Integer
-    return puts(time.strftime("%d-%m-%Y %H:%M:%S"))
+    return time.strftime("%d-%m-%Y %H:%M:%S")
   end
   future_time = time + (int*86400)
-  puts(future_time.strftime("%d-%m-%Y %H:%M:%S"))
+  future_time.strftime("%d-%m-%Y %H:%M:%S")
 end
 
 date_in_future()
