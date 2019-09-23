b6eb5ce091b0e7b02bd57f82a6acd38df039d2dd
DmitrBayan <55316650+DmitrBayan@users.noreply.github.com>
2019-09-23 19:38:18 +0300

    Update Task6.rb

--- Task6.rb
+++ Task6.rb
@@ -1,31 +1,31 @@
 def rps_game_winner(double_array)
   double_array.flatten!   #преобразование в одномерный
   if double_array.count >= 5  #если число вложенных масивов больше 5
-    return puts("WrongNumberOfPlayersError")
+    return ("WrongNumberOfPlayersError")
   elsif double_array[1] != 'P' and double_array[1] != 'R' and double_array[1] != 'S'  #ошибки в выборе хода
-    return puts("NoSuchStrategyError")
+    return ("NoSuchStrategyError")
   elsif double_array[3] != 'P' and double_array[3] != 'R' and double_array[3] != 'S'  #ошибки в выборе хода
-    return puts("NoSuchStrategyError")
+    return ("NoSuchStrategyError")
 
   elsif double_array[1] == 'P'    #стратегия, если первый выбросил "Бумагу"
     if double_array[3] == 'R' or double_array[3] == 'P'
-      puts("#{double_array[0]} #{double_array[1]}")
+      ("#{double_array[0]} #{double_array[1]}")
     else
-      puts("#{double_array[2]} #{double_array[3]}")
+      ("#{double_array[2]} #{double_array[3]}")
     end
 
   elsif double_array[1] == 'R'    #стратегия, если первый выбросил "Камень"
     if double_array[3] == 'R' or double_array[3] == 'S'
-      puts("#{double_array[0]} #{double_array[1]}")
+      ("#{double_array[0]} #{double_array[1]}")
     else
-      puts("#{double_array[2]} #{double_array[3]}")
+      ("#{double_array[2]} #{double_array[3]}")
     end
 
   elsif double_array[1] == 'S'    #стратегия, если первый выбросил "Ножницы"
     if double_array[3] == 'P' or double_array[3] == 'S'
-      puts("#{double_array[0]} #{double_array[1]}")
+      ("#{double_array[0]} #{double_array[1]}")
     else
-      puts("#{double_array[2]} #{double_array[3]}")
+      ("#{double_array[2]} #{double_array[3]}")
     end
   end
 
